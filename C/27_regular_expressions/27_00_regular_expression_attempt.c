05843113447595e17d8edd3ad5825251b6eb544f
ITWorks4U <nerd4classicgames@gmail.com>
2025-08-14 15:53:52 +0200

    added additional way for number check

--- C/27_regular_expressions/27_00_regular_expression_attempt.c
+++ C/27_regular_expressions/27_00_regular_expression_attempt.c
@@ -6,6 +6,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define INPUT_BUFFER 100
 
 int main(void) {
 	/*
@@ -37,6 +41,50 @@ int main(void) {
 		puts("Your input was not a number. :(");
 	} else {
 		puts("Your input was a number. :)");
+	}
+
+	//	appendix:
+	//	this also a "way" to check out, if a number has been typed in,
+	//	however, this is also surrounded with flaws
+
+	//	clean up input buffer => you may hit enter again
+	int c;
+	while ((c = getchar() != '\n' && c != EOF)) {}
+
+	char input[INPUT_BUFFER];
+	memset(input, '\0', INPUT_BUFFER);
+
+	printf("again: print any number: ");
+	fgets(input, (INPUT_BUFFER - 1), stdin);
+
+	//	remove trailing \n from fgets
+	size_t len = strlen(input);
+	if (len > 0 && input[len - 1] == '\n') {
+		input[len - 1] = '\0';
+	}
+
+	bool on_number = true;
+	printf("your input \"%s\" was ", input);
+
+	for (size_t i = 0; i < strlen(input); i++) {
+		//	your "number" may also be a negative number
+		//	if the first character is not "-" or in the range
+		//	between {0;9}, then this is any input rather
+		//	than a valid (negative) number
+		if (i == 0 && input[i] == '-') {
+			continue;
+		}
+
+		if ( input[i] < '0' || input[i] > '9') {
+			on_number = false;
+			break;
+		}
+	}
+
+	if (on_number) {
+		puts("a number");
+	} else {
+		puts("not a number");
 	}
 
 	return EXIT_SUCCESS;
