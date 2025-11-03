483ce42a23af858b0b06bb64f4311016c802e240
ITWorks4U <nerd4classicgames@gmail.com>
2025-11-03 10:18:43 +0100

    C++ programming tutorial

---
+++ C++/22_regular_expressions/22_00_using_c_regex.cpp
@@ -0,0 +1,13 @@
+/*
+* In C++ it makes no sense to use regular expressions with POSIX
+* or other external C libraries, like PCRE2, ...
+*
+* Attention:
+* A clean sample by using a regular expression with POSIX,
+* PCRE2 or by using a python helping script, take a look to:
+* https://github.com/ITWorks4U/programming_tutorials/tree/main/C/27_regular_expressions
+*/
+
+int main() {
+	return 0;
+}
